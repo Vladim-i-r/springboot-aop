ae6b8ec8da38a4bff20cdebb77d9644c4000265d
Vladim-i-r <vladimir.luna96@hotmail.com>
2024-03-02 23:25:32 -0600

    Add Aspect @After

--- src/main/java/com/vladimir/curso/springboot/app/aop/springbootaop/aop/GreetingAspect.java
+++ src/main/java/com/vladimir/curso/springboot/app/aop/springbootaop/aop/GreetingAspect.java
@@ -3,6 +3,7 @@ package com.vladimir.curso.springboot.app.aop.springbootaop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -20,6 +21,13 @@ public class GreetingAspect {
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs()); //Esto regresa un objeto pero con el arrays lo transformamos
         logger.info("Antes: " + method + " con los argumentos:" + args);
-    }  
+    } 
+
+    @After("execution(String com.vladimir.curso.springboot.app.aop.springbootaop.services.GreetingService.sayHello(..))")   //Si solo se pone la interfaz, engloba mas |  el (..) significa con los argumentos que sean
+    public void loggerAfter(JoinPoint joinPoint){          // une la ejecucion de un aspecto a la llamada de un metodo
     
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs()); //Esto regresa un objeto pero con el arrays lo transformamos
+        logger.info("Despues: " + method + " con los argumentos:" + args);
+    } 
 }
