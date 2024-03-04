42f562a121bb044424eb9e67682e7c0b4d63db7d
Vladimir Lopez <vladimir.luna96@hotmail.com>
2024-03-04 14:37:33 -0600

    Add @Order para administrar aspectos y @Pointcut para evitar repetir

--- src/main/java/com/vladimir/curso/springboot/app/aop/springbootaop/aop/GreetingAspect.java
+++ src/main/java/com/vladimir/curso/springboot/app/aop/springbootaop/aop/GreetingAspect.java
@@ -10,56 +10,65 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+@Order(2)
 @Aspect
 @Component
 public class GreetingAspect {
     private Logger logger =  LoggerFactory.getLogger(this.getClass()); //? Registra eventos
+
+                                                                                            //Si solo se pone la interfaz, engloba mas |  el (..) significa con los argumentos que sean
+    @Pointcut("execution(String com.vladimir.curso.springboot.app.aop.springbootaop.services.GreetingService.*(..))") //? Esto es un punto de corte general y se reutiliza en los demas aspectos para no repetir
+    private void greetingLoggerPointCut(){
+        
+    }
                     ////d |* con el asterisco regresa cualquier cosa                             *             *     
-    @Before("execution(String com.vladimir.curso.springboot.app.aop.springbootaop.services.GreetingService.sayHello(..))")   //Si solo se pone la interfaz, engloba mas |  el (..) significa con los argumentos que sean
+    @Before("greetingLoggerPointCut()")   
     public void loggerBefore(JoinPoint joinPoint){          // une la ejecucion de un aspecto a la llamada de un metodo
 
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs()); //Esto regresa un objeto pero con el arrays lo transformamos
-        logger.info("Antes: " + method + " con los argumentos:" + args);
+        logger.info("Order 2, Before: " + method + " con los argumentos:" + args);
     } 
 
-    @After("execution(String com.vladimir.curso.springboot.app.aop.springbootaop.services.GreetingService.sayHello(..))")   //Si solo se pone la interfaz, engloba mas |  el (..) significa con los argumentos que sean
+    @After("greetingLoggerPointCut()")   //
     public void loggerAfter(JoinPoint joinPoint){          // une la ejecucion de un aspecto a la llamada de un metodo
     
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs()); //Esto regresa un objeto pero con el arrays lo transformamos
-        logger.info("Despues: " + method + " con los argumentos:" + args);
+        logger.info("Order 2, After: " + method + " con los argumentos:" + args);
     } 
 
-    @AfterReturning("execution(String com.vladimir.curso.springboot.app.aop.springbootaop.services.GreetingService.sayHello(..))")   //Si solo se pone la interfaz, engloba mas |  el (..) significa con los argumentos que sean
+    @AfterReturning("greetingLoggerPointCut()")   //
     public void loggerAfterRet(JoinPoint joinPoint){          // une la ejecucion de un aspecto a la llamada de un metodo
     
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs()); //Esto regresa un objeto pero con el arrays lo transformamos
-        logger.info("Despues de retornar: " + method + " con los argumentos:" + args);
+        logger.info("After Returning: " + method + " con los argumentos:" + args);
     }
      
-    @AfterThrowing("execution(String com.vladimir.curso.springboot.app.aop.springbootaop.services.GreetingService.sayHello(..))")   //Si solo se pone la interfaz, engloba mas |  el (..) significa con los argumentos que sean
+    @AfterThrowing("execution(String com.vladimir.curso.springboot.app.aop.springbootaop.services.GreetingService.*(..))")   //! antes sayHello(..)) en lugar de *(..)) causaba que no entrara despues, por lo que se dejo con el *, mas general
     public void loggerAfterThr(JoinPoint joinPoint){          // une la ejecucion de un aspecto a la llamada de un metodo
     
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs()); //Esto regresa un objeto pero con el arrays lo transformamos
-        logger.info("Despues de lanzar la excepcion: " + method + " con los argumentos:" + args);
+        logger.info("After throwing exception: " + method + " con los argumentos:" + args);
     } 
 
-    @Around("execution(String com.vladimir.curso.springboot.app.aop.springbootaop.services.GreetingService.sayHello(..))")
+    @Around("greetingLoggerPointCut()")
     public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable{
         String method =  joinPoint.getSignature().getName();
         String args =Arrays.toString(joinPoint.getArgs());
         Object result = null;
         try {
-            logger.info("El metodo " + method + "() con los parametros: " + args);  //BEFORE
+            logger.info("Around-before, El metodo " + method + "() con los parametros: " + args);  //BEFORE
             result = joinPoint.proceed();
-            logger.info("El metodo " + method + "() retorna el resultado: " + result); //AFTER
+            logger.info("Around-after, El metodo " + method + "() retorna el resultado: " + result); //AFTER
             return result;
         } catch (Throwable e) {
             logger.error("Error en la llamada del metodo " + method + "()");
---
+++ src/main/java/com/vladimir/curso/springboot/app/aop/springbootaop/aop/GreetingFooAspect.java
@@ -0,0 +1,39 @@
+package com.vladimir.curso.springboot.app.aop.springbootaop.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)  // Intercepta en orden pero tambien segun el tipo del aspect.
+@Component
+@Aspect
+public class GreetingFooAspect {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Pointcut("execution(String com.vladimir.curso.springboot.app.aop.springbootaop.services.GreetingService.*(..))")
+    private void greetingFooAspectPointCut(){}
+
+    @Before("greetingFooAspectPointCut()")   //Si solo se pone la interfaz, engloba mas |  el (..) significa con los argumentos que sean
+    public void loggerBefore(JoinPoint joinPoint){          // une la ejecucion de un aspecto a la llamada de un metodo
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs()); //Esto regresa un objeto pero con el arrays lo transformamos
+        logger.info("Order 1, Before: " + method + " invocado con los parametros:" + args);
+    } 
+
+    @After("greetingFooAspectPointCut()")   //
+    public void loggerAfter(JoinPoint joinPoint){          // une la ejecucion de un aspecto a la llamada de un metodo
+    
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs()); //Esto regresa un objeto pero con el arrays lo transformamos
+        logger.info("Order 1, After: " + method + " con los argumentos:" + args);
+    } 
+}
